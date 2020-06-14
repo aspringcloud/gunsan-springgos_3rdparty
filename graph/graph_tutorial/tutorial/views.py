9e081042151cabbf29d51e19f5bba7a2521772f6
bcchoi <bcchoi@aspringcloud.com>
2020-06-14 11:41:54 +0900

    utc to kct, smach, logging, test token duration

--- graph/graph_tutorial/tutorial/views.py
+++ graph/graph_tutorial/tutorial/views.py
@@ -8,10 +8,89 @@ from tutorial.auth_helper import get_sign_in_url, get_token_from_code, store_tok
 from tutorial.graph_helper import get_user, get_calendar_events
 import dateutil.parser
 
+# utils
+import threading
+import time
+import logging
+# fsm
+import smach
+import py_trees
+# kafka
+import kafka
+logging.basicConfig(level=logging.INFO, format='%(asctime)s (%(threadName)-2s) %(message)s',
+                    )
+
+#===================== smach =========================
+
+def init_blackboard():
+  blackboard = py_trees.blackboard.Blackboard()
+  blackboard.pid = None
+  blackboard.dtg = {}
+  blackboard.event = {}
+  blackboard.osmurl = ''
+  blackboard.camurl = ''
+  return blackboard
+
+  
+def loginfo(msg):
+    logging.info(msg)
+
+def logwarn(msg):
+    logging.warning(msg)
+
+def logdebug(msg):
+    logging.debug(msg)
+
+def logerr(msg):
+    logging.error(msg)
+
+
+class preempted_timeout(smach.State):
+  def __init__(self, timeout):
+    smach.State.__init__(self, outcomes=['succeeded', 'preempted', 'aborted'],
+                          input_keys=['blackboard'],
+                          output_keys=['blackboard'])
+    self.timeout = timeout
+
+  def execute(self, ud):
+    start_time = time.time()
+    while True:
+        if self.preempt_requested():
+            self.service_preempt()
+            return 'preempted'
+        time.sleep(0.01)
+        if time.time() - start_time > self.timeout:
+            break
+    return 'succeeded'
+
+class dummy(smach.State):
+    def __init__(self, full_path, expected_state):
+        smach.State.__init__(self, outcomes=['succeeded', 'preempted', 'aborted'],
+                             input_keys=['blackboard'],
+                             output_keys=['blackboard'])
+        self.full_path = full_path
+        self.expected_state = expected_state
+
+    def execute(self, ud):
+        logging.info('dummy')
+        token = get_token_from_code(self.full_path, self.expected_state)
+        logging.inof(token)
+        return 'succeeded'
+
+def main(get_full_path, expected_state, token):
+  smach.set_loggers(loginfo,logwarn,logdebug,logerr) # disable
+  top = smach.StateMachine(outcomes=['succeeded', 'preempted', 'aborted'])
+  top.userdata.blackboard = init_blackboard()
+  with top:        
+      smach.StateMachine.add('start', preempted_timeout(5.0), {'succeeded': 'dummy'})
+      smach.StateMachine.add('dummy', dummy(get_full_path, expected_state), {'succeeded': 'succeeded'})
+      
+  outcome = top.execute()
+
+
 # <HomeViewSnippet>
 def home(request):
-  context = initialize_context(request)
-
+  context = initialize_context(request)  
   return render(request, 'tutorial/home.html', context)
 # </HomeViewSnippet>
 
@@ -44,8 +123,7 @@ def sign_in(request):
 # <SignOutViewSnippet>
 def sign_out(request):
   # Clear out the user and token
-  remove_user_and_token(request)
-
+  remove_user_and_token(request)  
   return HttpResponseRedirect(reverse('home'))
 # </SignOutViewSnippet>
 
@@ -53,8 +131,22 @@ def sign_out(request):
 def callback(request):
   # Get the state saved in session
   expected_state = request.session.pop('auth_state', '')
-  # Make the token request
+  # Make the token request  
   token = get_token_from_code(request.get_full_path(), expected_state)
+  
+  # start smach thread
+  logging.debug('Starting smach')
+  main_thread = threading.currentThread()
+  started = False
+  for t in threading.enumerate():
+    if t is main_thread:
+        continue
+    logging.info('joining %s', t.getName())
+    if t.getName() == 'smach':
+      started = True
+  if not started:
+    # 인자값으로 request해야 한다. 몇시간동안 하나의 token가지고 호출이 가능하므로 아래는 필요 없다. 
+    threading.Thread(name='smach', target=main, args=(request.get_full_path(), expected_state, token)).start()
 
   # Get the user's profile
   user = get_user(token)
@@ -85,3 +177,4 @@ def calendar(request):
 
   return render(request, 'tutorial/calendar.html', context)
 # </CalendarViewSnippet>
+
